bf4b4f1e3841d1c8506eeb30fa43f75bfe4106dd
praveen <praveenkmr.t@gmail.com>
2015-01-02 09:40:08 -0800

    My first layout

---
+++ gen/com/imrokraft/myfirstapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.imrokraft.myfirstapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/imrokraft/myfirstapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.imrokraft.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity {
+	public static String name ;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity3);
+		
+	}
+}
